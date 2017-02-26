6569537e1b341c8bc3873360bde1b155dd363533
Ben Baik <benbaik@Bens-MacBook-Pro.local>
2017-02-26 00:02:52 -0700

    trying to add delete

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ $(function() {
         getAllTodos: function() {
             return $.get('/todos/all').done(function(data) {
                 util.todoList = data;
-                util.init();
+                view.render();
             });
         },
         getActiveTodos: function() {
@@ -27,7 +27,8 @@ $(function() {
         addTodo: function(todo) {
             return $.post('/todos/all', this.createTodoObject(todo))
                 .done(function(data) {
-                    console.log(data);
+                    util.todoList.push(data);
+                    view.render();
                 });
         },
         deleteTodo: function(id) {
@@ -36,6 +37,7 @@ $(function() {
                 method: 'DELETE'
             }).done(function(data) {
                 console.log(data);
+                view.render();
             });
         },
         // edit OR toggle todos
@@ -51,16 +53,46 @@ $(function() {
     }
     const util = {
         init: function() {
+            todoInterface.getAllTodos();
             this.todoListSource = $('#todoListPre').html();
             this.todoListTemplate = Handlebars.compile(this.todoListSource);
+            view.bindEvents();
             view.render();
+        },
+        getAllTodos: function() {
+            todoInterface.getAllTodos();
+        },
+        addTodo: function(e) {
+            const input = e.target.value;
+            todoInterface.addTodo(input);
+            e.target.value = '';
+        },
+        deleteTodo: function(e) {
+            const elId = $(e.target).closest('li').data('id');
+            let index = util.getIndexFromEl(elId);
+            console.log(index);
+            //
+            // util.todoList.splice(index, 1);
+            // todoInterface.deleteTodo(elId);
+        },
+        getIndexFromEl: function(id) {
+            let i = this.todoList.length;
+            console.log(i);
+
+            while (i--) {
+                if (id === this.todoList[i].id) return i;
+            }
         }
     }
 
     const view = {
         render: function() {
             $('#todo-list').html(util.todoListTemplate(util.todoList));
+        },
+        bindEvents: function() {
+            $('#user-input').on('change', util.addTodo.bind(this));
+            $('#todo-list').on('click', '.delete', util.deleteTodo.bind(this));
         }
     }
-    todoInterface.getAllTodos();
+    util.init();
 });
