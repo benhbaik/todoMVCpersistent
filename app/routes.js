3b634d6b98a45c2345593782445000120c26e7b1
Ben Baik <benbaik@Bens-MacBook-Pro.local>
2017-02-26 17:23:44 -0700

    master toggle not working

--- app/routes.js
+++ app/routes.js
@@ -38,13 +38,25 @@ router.route('/active')
 router.route('/edit/:id')
     .put(function(req, res) {
         var query = { _id: req.params.id };
-        var update = { title: req.body.title, completed: req.body.completed };
+        var update = { title: req.body.title };
         var options = { 'new': true };
         Todo.findOneAndUpdate(query, update, options, function(err, todo) {
             if (err) res.status(500).json(err);
             else if (todo) res.status(200).json(todo);
         });
     });
+
+router.route('/toggle/:id')
+    .put(function(req, res) {
+        var query = { _id: req.params.id };
+        var update = { completed: req.body.completed };
+        var options = { 'new': true };
+        Todo.findOneAndUpdate(query, update, options, function(err, todo) {
+            if (err) res.status(500).json(err);
+            else if (todo) res.status(200).json(todo);
+        });
+    });
+
 router.route('/delete/:id')
     .delete(function(req, res) {
         var query = { _id: req.params.id };
--- public/app.js
+++ public/app.js
@@ -4,18 +4,20 @@ $(function() {
     const todoInterface = {
         getAllTodos: function() {
             return $.get('/todos/all').done(function(data) {
-                util.todoList = data;
+                util.todos = data;
+                util.setCompletedTodos();
+                util.setActiveTodos();
                 view.render();
             });
         },
         getActiveTodos: function() {
                 return $.get('/todos/active').done(function(data) {
-                    console.log(data);
+                    util.activeTodos = data;
                 });
         },
         getCompletedTodos: function() {
                 return $.get('/todos/completed').done(function(data) {
-                    console.log(data);
+                    util.completedTodos = data;
                 });
         },
         createTodoObject: function(todo) {
@@ -27,7 +29,7 @@ $(function() {
         addTodo: function(todo) {
             return $.post('/todos/all', this.createTodoObject(todo))
                 .done(function(data) {
-                    util.todoList.push(data);
+                    util.todos.push(data);
                     view.render();
                 });
         },
@@ -39,14 +41,33 @@ $(function() {
                 todoInterface.getAllTodos();
             });
         },
-        // edit OR toggle todos
-        editTodo: function(id, todo, status) {
-            $.ajax({
+        editTodo: function(id, todo) {
+            return $.ajax({
                 url: `/todos/edit/${id}`,
                 method: 'PUT',
-                data: { title: todo, completed: status }
+                data: { title: todo }
             }).done(function(data) {
-                console.log(data);
+                todoInterface.getAllTodos();
+            });
+        },
+        toggleTodo: function(id, status) {
+            return $.ajax({
+                url: `/todos/toggle/${id}`,
+                method: 'PUT',
+                data: { completed: status }
+            }).done(function(data) {
+                todoInterface.getAllTodosSynced(data.completed);
+            });
+        },
+        getAllTodosSynced: function(lastTodoStatus) {
+            return $.get('/todos/all').done(function(data) {
+                util.todos = data;
+                util.setCompletedTodos();
+                if (lastTodoStatus && util.todos.length === util.completedTodos.length) {
+                    util.triggerMasterToggle(lastTodoStatus);
+                }
+                util.triggerMasterToggle(lastTodoStatus);
+                view.render();
             });
         }
     }
@@ -60,6 +81,12 @@ $(function() {
         getAllTodos: function() {
             todoInterface.getAllTodos();
         },
+        getActiveTodos: function() {
+            todoInterface.getActiveTodos();
+        },
+        getCompletedTodos: function() {
+            todoInterface.getCompletedTodos();
+        },
         addTodo: function(e) {
             const input = e.target.value;
             todoInterface.addTodo(input);
@@ -68,16 +95,62 @@ $(function() {
         deleteTodo: function(e) {
             const elId = $(e.target).closest('li').data('id');
             todoInterface.deleteTodo(elId);
+        },
+        activateEdit: function(e) {
+            const editInput = $(e.target).closest('li').find('.edit');
+            editInput.removeClass('edit').addClass('editing');
+            editInput.val(editInput.val()).focus();
+        },
+        editTodo: function(e) {
+            const todoId = $(e.target).closest('li').data('id');
+            const newTodo = e.target.value;
+            todoInterface.editTodo(todoId, newTodo);
+        },
+        toggleTodo: function(e) {
+            const todoId = $(e.target).closest('li').data('id');
+            const todoStatus = $(e.target).prop('checked');
+            todoInterface.toggleTodo(todoId, todoStatus);
+        },
+        toggleAll: function(e) {
+            // make a toggle all to completed and all to active api route???
+            // master toggle not working
+            // if todos are not all true make them all true
+            // if todos are all true make them false
+            if (util.completedTodos < util.todos) {
+
+            }
+            if (util.activeTodos === util.todos) {
+                $(e.target).prop('checked', false);
+            }
+        },
+        setCompletedTodos: function() {
+            util.completedTodos = util.todos.filter(isItCompleted);
+            function isItCompleted(todo) {
+                if (todo.completed === true) return true;
+            }
+        },
+        setActiveTodos: function() {
+            util.activeTodos = util.todos.filter(isItActive);
+            function isItActive(todo) {
+                if (todo.completed === false) return true;
+            }
+        },
+        triggerMasterToggle: function(lastTodoStatus) {
+            $('#toggle-all').prop('checked', lastTodoStatus);
         }
     }
 
     const view = {
         render: function() {
-            $('#todo-list').html(util.todoListTemplate(util.todoList));
+            $('#todo-list').html(util.todoListTemplate(util.todos));
         },
         bindEvents: function() {
             $('#user-input').on('change', util.addTodo.bind(this));
-            $('#todo-list').on('click', '.delete', util.deleteTodo.bind(this));
+            $('#toggle-all').on('click', util.toggleAll.bind(this));
+            $('#todo-list').on('click', '.delete', util.deleteTodo.bind(this))
+                .on('click', '.toggle', util.toggleTodo.bind(this))
+                .on('dblclick', 'li', util.activateEdit.bind(this))
+                .on('change', '.editing', util.editTodo.bind(this));
         }
     }
     util.init();
